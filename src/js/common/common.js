67e032b503507dc846a760f47f8b9d1c2b3a9cb6
arai <arai.shg717@gmail.com>
2019-05-04 12:23:26 +0900

    js練習

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -1,4 +1,9 @@
 {
-
-
+  console.log(
+    Array.from(document.querySelectorAll('a')).map((x) => {
+      x.style.display = 'block'
+      x.style.color = 'green'
+      x.style.fontWeight = 'bold'
+    })
+  )
 };
